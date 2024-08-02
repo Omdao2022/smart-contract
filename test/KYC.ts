7540e771b116c8d9a3cb46cc7978edf41806c108
hcrypto7 <levilee9571@gmail.com>
2024-08-02 15:52:51 +1000

    feat: contract testcode added

--- test/KYC.ts
+++ test/KYC.ts
@@ -7,5 +7,50 @@ import { expect } from "chai";
 import hre from "hardhat";
 
 describe("KYC", function () {
-  
+  async function deployKycFixture() {
+    const [owner, otherAccount] = await hre.ethers.getSigners();
+    const KYC = await hre.ethers.getContractFactory("KYC");
+    const kyc = await KYC.deploy();
+    return { kyc, owner, otherAccount };
+  }
+
+  describe("KYC approve", function () {
+    it("every wallet address shouldn't be kyc verified", async function () {
+      const { kyc, otherAccount, owner } = await loadFixture(deployKycFixture);
+      expect(await kyc.isKYCPassed(otherAccount.address)).to.equal(false);
+    });
+
+    it("verify a wallet address", async function () {
+      const { kyc, otherAccount, owner } = await loadFixture(deployKycFixture);
+      await kyc.approveKYC(otherAccount.address);
+      expect(await kyc.isKYCPassed(otherAccount.address)).to.equal(true);
+    });
+
+    it("only owner can approve KYC", async function () {
+      const { kyc, otherAccount, owner } = await loadFixture(deployKycFixture);
+      await expect(kyc.connect(otherAccount).approveKYC(otherAccount.address)).to.be.revertedWith(
+        "Not authorized"
+      );
+    });
+  });
+
+  describe("Transfer Ownership", function () {
+    it("original owner should be equal with deployer address", async function () {
+      const { kyc, otherAccount, owner } = await loadFixture(deployKycFixture);
+      await expect(await kyc.owner()).to.be.equal(owner.address);
+    });
+
+    it("Should be transfer ownership to other account", async function () {
+      const { kyc, otherAccount, owner } = await loadFixture(deployKycFixture);
+      await kyc.transferOwner(otherAccount.address);
+      expect(await kyc.owner()).to.be.equal(otherAccount.address);
+    });
+
+    it("Only owner can transfer ownership", async function () {
+      const { kyc, otherAccount, owner } = await loadFixture(deployKycFixture);
+      await expect(kyc.connect(otherAccount).transferOwner(otherAccount)).to.be.revertedWith("Not authorized");
+    });
+  })
+
+
 });
