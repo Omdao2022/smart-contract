0615800397cb9a3edce3ff701c41cc56b12fc3a4
hcrypto7 <levilee9571@gmail.com>
2024-08-05 11:22:20 -0500

    feat: eip712 signature

--- contracts/KYC.sol
+++ contracts/KYC.sol
@@ -1,45 +1,62 @@
-// SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.24;
 
-// Uncomment this line to use console.log
+import "@openzeppelin/contracts-upgradeable/proxy/utils/Initializable.sol";
+import "@openzeppelin/contracts-upgradeable/utils/cryptography/ECDSAUpgradeable.sol";
+
 import "hardhat/console.sol";
 
-contract KYC {
-  address public owner;
+contract KYC is Initializable {
+    using ECDSAUpgradeable for bytes32;
 
-  modifier onlyOwner() {
-    require(msg.sender == owner, "Not authorized");
-    _;
-  }
+    // EIP-712 Domain separator
+    bytes32 public DOMAIN_SEPARATOR;
 
-  mapping(address => bool) public kycPassed;
+    // EIP-712 typehash for the setKYC function
+    bytes32 public constant SET_KYC_TYPEHASH =
+        keccak256("SetKYC(address user)");
 
-  event KYCApproved(address indexed wallet);
-  event KYCRevoked(address indexed wallet);
+    function initialize(
+        string memory name,
+        string memory version
+    ) public initializer {
+        DOMAIN_SEPARATOR = keccak256(
+            abi.encode(
+                keccak256(
+                    "EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"
+                ),
+                keccak256(bytes(name)),
+                keccak256(bytes(version)),
+                block.chainid,
+                address(this)
+            )
+        );
+    }
 
-  constructor() {
-    owner = msg.sender;
-  }
+    function setKYC(
+        address user,
+        bytes memory signature
+    ) public view returns (bool) {
+        // Construct the digest
+        bytes32 digest = keccak256(
+            abi.encodePacked(
+                "\x19\x01",
+                DOMAIN_SEPARATOR,
+                keccak256(abi.encode(SET_KYC_TYPEHASH, user))
+            )
+        );
 
-  // To be called by backend after KYC approval
-  function approveKYC(address _wallet) public onlyOwner {
-    kycPassed[_wallet] = true;
-    emit KYCApproved(_wallet);
-  }
+        // Recover address from signature and verify
+        // return recoverSigner(digest, signature) == msg.sender;
+        address recover = recoverSigner(digest, signature);
+        console.log("recover==========>%s, msg.sender===========>%s", recover, msg.sender);
+        return recover == msg.sender;
+    }
 
-  // To be called by backend after KYC revoke
-  function revokeKYC(address _wallet) public onlyOwner {
-    kycPassed[_wallet] = false;
-    emit KYCRevoked(_wallet);
-  }
-
-  // Function to check if a wallet has passed KYC
-  function isKYCPassed(address _wallet) public view returns (bool) {
-    return kycPassed[_wallet];
-  }
-
-  function transferOwner(address _newOwner) public onlyOwner {
-    require(_newOwner != address(0), "New owner can't be zero address");
-    owner = _newOwner;
-  }
+    function recoverSigner(
+        bytes32 digest,
+        bytes memory signature
+    ) internal pure returns (address) {
+        return digest.recover(signature);
+    }
 }
--- test/KYC.ts
+++ test/KYC.ts
@@ -6,73 +6,170 @@ import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
 import { expect } from "chai";
 import hre from "hardhat";
 
-describe("KYC", function () {
+// describe("KYC", function () {
+//   async function deployKycFixture() {
+//     const [owner, otherAccount] = await hre.ethers.getSigners();
+//     const KYC = await hre.ethers.getContractFactory("KYC");
+//     const kyc = await KYC.deploy();
+//     return { kyc, owner, otherAccount };
+//   }
+
+//   describe("KYC approve", function () {
+//     it("every wallet address shouldn't be kyc verified", async function () {
+//       const { kyc, otherAccount, owner } = await loadFixture(deployKycFixture);
+//       expect(await kyc.isKYCPassed(otherAccount.address)).to.equal(false);
+//     });
+
+//     it("verify a wallet address", async function () {
+//       const { kyc, otherAccount, owner } = await loadFixture(deployKycFixture);
+//       await kyc.approveKYC(otherAccount.address);
+//       expect(await kyc.isKYCPassed(otherAccount.address)).to.equal(true);
+//     });
+
+//     it("only owner can approve KYC", async function () {
+//       const { kyc, otherAccount, owner } = await loadFixture(deployKycFixture);
+//       await expect(
+//         kyc.connect(otherAccount).approveKYC(otherAccount.address)
+//       ).to.be.revertedWith("Not authorized");
+//     });
+//   });
+
+//   describe("KYC revoke", function () {
+//     it("every wallet address shouldn't be kyc verified", async function () {
+//       const { kyc, otherAccount, owner } = await loadFixture(deployKycFixture);
+//       expect(await kyc.isKYCPassed(otherAccount.address)).to.equal(false);
+//     });
+
+//     it("verify a wallet address and revoke", async function () {
+//       const { kyc, otherAccount, owner } = await loadFixture(deployKycFixture);
+//       await kyc.approveKYC(otherAccount.address);
+//       expect(await kyc.isKYCPassed(otherAccount.address)).to.equal(true);
+//       await kyc.revokeKYC(otherAccount.address);
+//       expect(await kyc.isKYCPassed(otherAccount.address)).to.equal(false);
+//     });
+
+//     it("only owner can revoke KYC", async function () {
+//       const { kyc, otherAccount, owner } = await loadFixture(deployKycFixture);
+//       await expect(
+//         kyc.connect(otherAccount).revokeKYC(otherAccount.address)
+//       ).to.be.revertedWith("Not authorized");
+//     });
+//   });
+
+//   describe("Transfer Ownership", function () {
+//     it("original owner should be equal with deployer address", async function () {
+//       const { kyc, otherAccount, owner } = await loadFixture(deployKycFixture);
+//       await expect(await kyc.owner()).to.be.equal(owner.address);
+//     });
+
+//     it("Should be transfer ownership to other account", async function () {
+//       const { kyc, otherAccount, owner } = await loadFixture(deployKycFixture);
+//       await kyc.transferOwner(otherAccount.address);
+//       expect(await kyc.owner()).to.be.equal(otherAccount.address);
+//     });
+
+//     it("Only owner can transfer ownership", async function () {
+//       const { kyc, otherAccount, owner } = await loadFixture(deployKycFixture);
+//       await expect(
+//         kyc.connect(otherAccount).transferOwner(otherAccount)
+//       ).to.be.revertedWith("Not authorized");
+//     });
+//   });
+// });
+
+describe("KYC Contract", function () {
+  const NAME = "KYC";
+  const VERSION = "1";
   async function deployKycFixture() {
     const [owner, otherAccount] = await hre.ethers.getSigners();
     const KYC = await hre.ethers.getContractFactory("KYC");
-    const kyc = await KYC.deploy();
+    // const kyc = await KYC.deploy();
+    const kyc = await hre.upgrades.deployProxy(KYC, ["KYC", "1"], {
+      initializer: "initialize",
+    });
     return { kyc, owner, otherAccount };
   }
-
-  describe("KYC approve", function () {
-    it("every wallet address shouldn't be kyc verified", async function () {
+  describe("setKYC", function () {
+    it("Should initialize the DOMAIN_SEPARATOR correctly", async function () {
       const { kyc, otherAccount, owner } = await loadFixture(deployKycFixture);
-      expect(await kyc.isKYCPassed(otherAccount.address)).to.equal(false);
+      const abiCoder = new hre.ethers.AbiCoder();
+      console.log(
+        "abiCoder=======>",
+        await kyc.getAddress(),
+        hre.network.config.chainId
+      );
+      const expectedDomainSeparator = hre.ethers.keccak256(
+        abiCoder.encode(
+          ["bytes32", "bytes32", "bytes32", "uint256", "address"],
+          [
+            hre.ethers.keccak256(
+              hre.ethers.toUtf8Bytes(
+                "EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"
+              )
+            ),
+            hre.ethers.keccak256(hre.ethers.toUtf8Bytes(NAME)),
+            hre.ethers.keccak256(hre.ethers.toUtf8Bytes(VERSION)),
+            await hre.network.config.chainId,
+            await kyc.getAddress(),
+          ]
+        )
+      );
+
+      console.log("SetKYC:", expectedDomainSeparator);
+
+      expect(await kyc.DOMAIN_SEPARATOR()).to.equal(expectedDomainSeparator);
     });
 
-    it("verify a wallet address", async function () {
-      const { kyc, otherAccount, owner } = await loadFixture(deployKycFixture);
-      await kyc.approveKYC(otherAccount.address);
-      expect(await kyc.isKYCPassed(otherAccount.address)).to.equal(true);
-    });
+    it("Should verify KYC correctly", async function () {
+      const { kyc, owner, otherAccount } = await loadFixture(deployKycFixture);
+      const user = otherAccount.address;
+      const DOMAIN_SEPARATOR = await kyc.DOMAIN_SEPARATOR();
+      const SET_KYC_TYPEHASH = await kyc.SET_KYC_TYPEHASH();
 
-    it("only owner can approve KYC", async function () {
-      const { kyc, otherAccount, owner } = await loadFixture(deployKycFixture);
-      await expect(
-        kyc.connect(otherAccount).approveKYC(otherAccount.address)
-      ).to.be.revertedWith("Not authorized");
-    });
-  });
+      // Create the digest
 
-  describe("KYC revoke", function () {
-    it("every wallet address shouldn't be kyc verified", async function () {
-      const { kyc, otherAccount, owner } = await loadFixture(deployKycFixture);
-      expect(await kyc.isKYCPassed(otherAccount.address)).to.equal(false);
-    });
+      // Create the digest
+      const abiCoder = await new hre.ethers.AbiCoder();
+      const digest = hre.ethers.keccak256(
+        hre.ethers.solidityPacked(
+          ["bytes1", "bytes1", "bytes32", "bytes32"],
+          [
+            "0x19",
+            "0x01",
+            DOMAIN_SEPARATOR,
+            hre.ethers.keccak256(
+              abiCoder.encode(
+                ["bytes32", "address"],
+                [SET_KYC_TYPEHASH, user]
+              )
+            ),
+          ]
+        )
+      );
 
-    it("verify a wallet address and revoke", async function () {
-      const { kyc, otherAccount, owner } = await loadFixture(deployKycFixture);
-      await kyc.approveKYC(otherAccount.address);
-      expect(await kyc.isKYCPassed(otherAccount.address)).to.equal(true);
-      await kyc.revokeKYC(otherAccount.address);
-      expect(await kyc.isKYCPassed(otherAccount.address)).to.equal(false);
-    });
+      const domain = {
+        name: "KYC",
+        version: "1",
+        chainId: await hre.network.config.chainId,
+        verifyingContract: await kyc.getAddress(),
+      };
 
-    it("only owner can revoke KYC", async function () {
-      const { kyc, otherAccount, owner } = await loadFixture(deployKycFixture);
-      await expect(
-        kyc.connect(otherAccount).revokeKYC(otherAccount.address)
-      ).to.be.revertedWith("Not authorized");
-    });
-  });
+      const types = {
+        SetKYC: [{ name: "user", type: "address" }],
+      };
 
-  describe("Transfer Ownership", function () {
-    it("original owner should be equal with deployer address", async function () {
-      const { kyc, otherAccount, owner } = await loadFixture(deployKycFixture);
-      await expect(await kyc.owner()).to.be.equal(owner.address);
-    });
+      const value = {
+        user: otherAccount.address,
+      };
 
-    it("Should be transfer ownership to other account", async function () {
-      const { kyc, otherAccount, owner } = await loadFixture(deployKycFixture);
-      await kyc.transferOwner(otherAccount.address);
-      expect(await kyc.owner()).to.be.equal(otherAccount.address);
-    });
+      const signature = await owner.signTypedData(domain, types, value);
 
-    it("Only owner can transfer ownership", async function () {
-      const { kyc, otherAccount, owner } = await loadFixture(deployKycFixture);
-      await expect(
-        kyc.connect(otherAccount).transferOwner(otherAccount)
-      ).to.be.revertedWith("Not authorized");
+      // Sign the digest
+      // const signature = await owner.signMessage(hre.ethers.getBytes(digest));
+
+      // Verify KYC
+      const result = await kyc.setKYC(user, signature);
+      expect(result).to.be.true;
     });
   });
 });
